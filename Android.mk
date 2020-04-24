179008a6ffb9a53756ae4e5e3247ce9360e0313f
ZysGit <532683360@qq.com>
2020-04-24 17:21:16 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -36,3 +36,35 @@ $(call import-module,.)
 # Android.mk 全解： https://developer.android.com/ndk/guides/android_mk
 # Android NDK 详解： https://developer.android.com/ndk
 # ndk-build 命令详解：https://developer.android.com/ndk/guides/ndk-build
+
+
+# =============================常用自定义脚本================================
+# 配置自己的源文件目录和源文件后缀名
+MY_FILES_PATH  :=  $(LOCAL_PATH) \
+                   $(LOCAL_PATH)/../../Classes
+
+MY_FILES_SUFFIX := %.cpp %.c
+
+# 递归遍历目录下的所有的文件
+rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+# 获取相应的源文件
+MY_ALL_FILES := $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*.*) ) 
+MY_ALL_FILES := $(MY_ALL_FILES:$(MY_CPP_PATH)/./%=$(MY_CPP_PATH)%)
+MY_SRC_LIST  := $(filter $(MY_FILES_SUFFIX),$(MY_ALL_FILES)) 
+MY_SRC_LIST  := $(MY_SRC_LIST:$(LOCAL_PATH)/%=%)
+
+# 去除字串的重复单词
+define uniq =
+  $(eval seen :=)
+  $(foreach _,$1,$(if $(filter $_,${seen}),,$(eval seen += $_)))
+  ${seen}
+endef
+
+# 递归遍历获取所有目录
+MY_ALL_DIRS := $(dir $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*/) ) )
+MY_ALL_DIRS := $(call uniq,$(MY_ALL_DIRS))
+
+# 赋值给NDK编译系统
+LOCAL_SRC_FILES  := $(MY_SRC_LIST)
+LOCAL_C_INCLUDES := $(MY_ALL_DIRS)
